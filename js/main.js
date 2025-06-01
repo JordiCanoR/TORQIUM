6589b434e86ee5ab4a73696c758b9dec35515f83
Jordi Cano Román <jordicanoroman@gmail.com>
2025-06-01 10:54:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-
 function ocultarIntro() {
-  document.getElementById("intro-video").style.display = "none";
-  document.getElementById("pantalla-inicio").style.display = "flex";
-  document.body.style.overflow = "hidden"; // para evitar scroll
+  const intro = document.getElementById("intro-video");
+  const contenido = document.getElementById("contenido");
+
+  if (intro) intro.style.display = "none";
+  if (contenido) contenido.style.display = "block";
 }
